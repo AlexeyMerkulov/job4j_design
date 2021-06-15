9dac3b06ebff6c715cedd5950d0d7bad82bc607f
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2021-06-15 21:01:22 +0300

    1. Бот [#7921 #167721](исправление)

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,22 +13,16 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String input = in.readLine();
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     if (input != null && !input.isEmpty()) {
                         System.out.println(input);
                         String answer = input.split("[= ]")[2];
                         switch (answer) {
                             case "Exit" -> server.close();
-                            case "Hello" -> {
-                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                                out.write("Hello".getBytes());
-                            }
-                            default -> {
-                                out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                                out.write("What".getBytes());
-                            }
+                            case "Hello" -> out.write("Hello".getBytes());
+                            default -> out.write("What".getBytes());
                         }
                     } else {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         out.write("Enter not empty phrase".getBytes());
                     }
                 }

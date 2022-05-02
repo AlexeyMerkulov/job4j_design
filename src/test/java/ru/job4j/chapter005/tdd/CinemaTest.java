5989b5cefc92be28d18a3cdd2573f57872488abf
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-05-02 15:53:12 +0300

    2. Что такое TDD? [#4918](исправление)

--- src/test/java/ru/job4j/chapter005/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/chapter005/tdd/CinemaTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.chapter005.tdd;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -12,7 +13,8 @@ import static org.hamcrest.Matchers.is;
 
 public class CinemaTest {
 
-    // @Test
+    @Ignore
+    @Test
     public void whenBuy() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -22,7 +24,8 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
-    // @Test
+    @Ignore
+    @Test
     public void whenFind() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
@@ -30,14 +33,16 @@ public class CinemaTest {
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
 
-    // @Test
+    @Ignore
+    @Test
     public void findEmpty() {
         Cinema cinema = new Cinema3D();
         List<Session> sessions = cinema.find(session -> true);
         assertTrue(sessions.size() == 0);
     }
 
-    // @Test(expected = IllegalArgumentException.class)
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
     public void whenBuyThenIllegalDate() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -46,12 +51,24 @@ public class CinemaTest {
         Ticket ticket = cinema.buy(account, 1, 1, date);
     }
 
-    // @Test(expected = IllegalArgumentException.class)
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
     public void whenBuyThenIllegalSeat() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 38, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuySameSeat() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket sameTicket = cinema.buy(account, 1, 1, date);
     }
 }
